9bbec8b48ee9d545358e699efe98cddc86ac4bee
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-01-07 21:25:20 +0100

    7-Consumo del servicio web por consola

---
+++ Consumo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.ServiciosSoapClient client = new ServiceReference1.ServiciosSoapClient();
+
+            string result = client.HelloWorld();
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
